3880fb443a16f116063d88029b512a3335f14f14
Luke Iannucci <luke.iannucci@outlook.com>
2025-04-28 19:20:28 -0400

    short circuit

--- run-nitro-test-node/waitForNitro.sh
+++ run-nitro-test-node/waitForNitro.sh
@@ -2,6 +2,11 @@
 
 PORT=${1:-8547}
 
+if [ "$PORT" = "3347" ]; then
+  echo "Port is 3347, skipping check and exiting with success"
+  exit 0
+fi
+
 while true
 do
   curl -X POST -H 'Content-Type: application/json' -d '{"jsonrpc":"2.0","id":45678,"method":"eth_chainId","params":[]}' "http://localhost:$PORT"
